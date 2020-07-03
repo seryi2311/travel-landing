e460bcf3de07e287978c4522b314f94571638c21
Ваше Имя <you@example.com>
2020-07-03 07:39:39 +0300

    add mobile version

--- source/js/mainjs.js
+++ source/js/mainjs.js
@@ -1,3 +1,17 @@
+(function () {
+  const header = document.querySelector('.header');
+  const active = document.querySelector('.header_container');
+  window.onscroll = () => {
+    if (window.pageYOffset > 100) {
+      header.classList.add('header_active');
+      active.classList.add('header_container_active');
+    } else {
+      header.classList.remove('header_active');
+      active.classList.remove('header_container_active');
+    }
+  };
+}());
+
 (function(){
   const burgerItem = document.querySelector('.burger');
   const menu = document.querySelector('.nav');
